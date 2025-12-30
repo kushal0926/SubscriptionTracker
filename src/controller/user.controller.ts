7e3fdba276fd5db77072e5b2043d85f6df7395d7
kushal tamang <kushaltamang0926@gmail.com>
2025-12-30 21:09:49 +0545

    some changes

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -20,7 +20,7 @@ export const getUsers = async (
   try {
     const users = await User.find();
 
-    res.status(200).json({ success: true, data: users });
+    res.status(200).json({ success: true, data:  users });
   } catch (error) {
     next(error);
   }
